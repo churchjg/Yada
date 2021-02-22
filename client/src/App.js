300e86af5fc94343274e1b3d393ed3b756dd482f
Churchjg <churchjg283@gmail.com>
2021-02-22 16:09:07 -0500

    running

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Chat from './components/Chat'
+import Join from './components/Join'
+
 const App = () => {
   return (
     <Router>
