59bbf78a44d448ab2463a84a0d820c246b2cb94f
Churchjg <churchjg283@gmail.com>
2021-02-22 19:22:54 -0500

    backend done

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Yada Yada Yada ... <span role="img" aria-label="emoji">💬</span></h1>
+      
+      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
+
+      <h6>Created with React, Express, Node, and Socket.IO <span role="img" aria-label="emoji"></span></h6>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
